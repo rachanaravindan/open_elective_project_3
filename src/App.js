9d5481aafa693ce601e7c801d04c2b1c069ae51e
sharandeepak <sharandeepak32@gmail.com>
2022-10-11 21:51:39 +0530

    init commit /sharan

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sharan is a good boy !!</h1>
+    </div>
+  );
+}
+
+export default App;
